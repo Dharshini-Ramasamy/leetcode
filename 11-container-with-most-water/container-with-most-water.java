34552d92f51649d8a9affd73a4ae904a947c7c3f
Dharshini-Ramasamy <dharshiniramasamyy@gmail.com>
2026-08-05 22:05:37 +0530

    Time: 5 ms (82.96%) | Memory: 77.6 MB (12.78%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -3,7 +3,7 @@ class Solution {
         int start=0;
         int end=height.length-1;
         int max=0;
-        while(start<=end){
+        while(start<end){
             
                 int h=Math.min(height[start],height[end]);
                 int width=end-start;
