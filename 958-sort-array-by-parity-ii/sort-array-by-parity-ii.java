0a46ee1034e9251ad6096db7a0a892976c7c0e5b
Dharshini-Ramasamy <dharshiniramasamyy@gmail.com>
2026-08-08 11:33:33 +0530

    Time: 3 ms (57.23%) | Memory: 48.9 MB (34.97%) - LeetSync

--- 958-sort-array-by-parity-ii/sort-array-by-parity-ii.java
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.java
@@ -4,16 +4,18 @@ class Solution {
         int odd=1;
         int arr[]=new int[nums.length];
         for(int i=0;i<nums.length;i++){
+            if(even<nums.length){
             if(nums[i]%2==0){
                 arr[even]=nums[i];
                 even+=2;
-            }
-            else{
+            }}
+            if(nums[i]%2==1){
+                if(odd<nums.length){
                 arr[odd]=nums[i];
                 odd+=2;
-            }
+            }}
             
         }
         return arr;
     }
-}
+    }
