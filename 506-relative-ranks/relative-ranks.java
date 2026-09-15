e6b4064008eecf8c23780a01f236f6144ce84aa5
Dharshini-Ramasamy <dharshiniramasamyy@gmail.com>
2026-09-15 22:24:19 +0530

    Time: 126 ms (5.09%) | Memory: 47.2 MB (92.85%) - LeetSync

--- 506-relative-ranks/relative-ranks.java
+++ 506-relative-ranks/relative-ranks.java
@@ -2,47 +2,25 @@ class Solution {
     public String[] findRelativeRanks(int[] score) {
         String[] arr=new String[score.length];
         for(int i=0;i<score.length;i++){
-            arr[i]=String.valueOf(score[i]);
+          int count=1;
+          for(int j=0;j<score.length;j++){
+            if(score[j]>score[i]){
+                count++;
+            }
+          }
+          if(count==1){
+            arr[i]="Gold Medal";
+          }
+          else if(count==2){
+            arr[i]="Silver Medal";
+          }
+          else if(count==3){
+            arr[i]="Bronze Medal";
+          }
+          else{
+            arr[i]=String.valueOf(count);
+          }
         }
-        int m=Integer.MIN_VALUE;
-        int d=Integer.MIN_VALUE;
-        int v=Integer.MIN_VALUE;
-        for(int i=0;i<score.length;i++){
-            if(score[i]>m){
-                v=d;
-                d=m;
-                m=score[i];
-            }
-            else if(score[i]<m&&score[i]>d){
-                v=d;
-                d=score[i];
-            }
-            else if(score[i]<d&&score[i]>v){
-                v=score[i];
-            }
-        }
-        for(int i=0;i<score.length;i++){
-            if(score[i]==m){
-                arr[i]="Gold Medal";
-            }
-            else if(score[i]==d){
-                arr[i]="Silver Medal";
-            }
-            else if(score[i]==v){
-                arr[i]="Bronze Medal";
-            }
-            else{
-               int count=1;
-               for(int j=0;j<score.length;j++){
-                if(score[j]>score[i]){
-                    count++;
-                }
-               }
-               arr[i]=String.valueOf(count);
-            }
-        }
-        return arr;
-
-        
+        return arr;        
     }
 }
