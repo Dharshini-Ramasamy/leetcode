cf9c923273cb754d820d473b4905a96435cdd4d1
Dharshini-Ramasamy <dharshiniramasamyy@gmail.com>
2026-09-16 22:06:36 +0530

    Time: 6 ms (1.46%) | Memory: 45 MB (8.18%) - LeetSync

--- 3553-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.java
+++ 3553-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.java
@@ -1,7 +1,34 @@
 class Solution {
     public boolean checkTwoChessboards(String coordinate1, String coordinate2) {
-        int s1 = coordinate1.charAt(0) + coordinate1.charAt(1);
-        int s2 = coordinate2.charAt(0) + coordinate2.charAt(1);
-        return (s1 % 2) == (s2 % 2);
+
+        String[] arr1 = coordinate1.split("");
+        String[] arr2 = coordinate2.split("");
+
+        int n = Integer.parseInt(arr1[1]) % 2;
+        int m = Integer.parseInt(arr2[1]) % 2;
+
+        boolean first = arr1[0].equals("a") ||
+                        arr1[0].equals("c") ||
+                        arr1[0].equals("e") ||
+                        arr1[0].equals("g");
+
+        boolean second = arr2[0].equals("a") ||
+                         arr2[0].equals("c") ||
+                         arr2[0].equals("e") ||
+                         arr2[0].equals("g");
+
+        if (first && second) {
+            return n == m;
+        }
+
+        if (!first && !second) {
+            return n == m;
+        }
+
+        if (first && !second) {
+            return n != m;
+        }
+
+        return n != m;
     }
 }
