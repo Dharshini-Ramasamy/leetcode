8e06959bf5f27ea6c7cc24cba79f4768eeac4d68
Dharshini-Ramasamy <dharshiniramasamyy@gmail.com>
2026-09-16 22:10:11 +0530

    Time: 6 ms (1.46%) | Memory: 44.6 MB (8.18%) - LeetSync

--- 3553-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.java
+++ 3553-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.java
@@ -1,34 +1,30 @@
 class Solution {
     public boolean checkTwoChessboards(String coordinate1, String coordinate2) {
-
-        String[] arr1 = coordinate1.split("");
-        String[] arr2 = coordinate2.split("");
-
-        int n = Integer.parseInt(arr1[1]) % 2;
-        int m = Integer.parseInt(arr2[1]) % 2;
-
-        boolean first = arr1[0].equals("a") ||
-                        arr1[0].equals("c") ||
-                        arr1[0].equals("e") ||
-                        arr1[0].equals("g");
-
-        boolean second = arr2[0].equals("a") ||
-                         arr2[0].equals("c") ||
-                         arr2[0].equals("e") ||
-                         arr2[0].equals("g");
-
-        if (first && second) {
-            return n == m;
+        String[] arr1=coordinate1.split("");
+        String[] arr2=coordinate2.split("");
+        int n=Integer.parseInt(arr1[1])%2;
+        int m=Integer.parseInt(arr2[1])%2;
+        if((arr1[0].equals("a")||arr1[0].equals("c")||arr1[0].equals("e")||arr1[0].equals("g"))&&(arr2[0].equals("a")||arr2[0].equals("c")||arr2[0].equals("e")||arr2[0].equals("g"))){
+            if(n==m){
+                return true;
+            }
         }
-
-        if (!first && !second) {
-            return n == m;
+       else if((arr1[0].equals("b")||arr1[0].equals("d")||arr1[0].equals("f")||arr1[0].equals("h"))&&(arr2[0].equals("b")||arr2[0].equals("d")||arr2[0].equals("f")||arr2[0].equals("h"))){
+            if(n==m){
+                return true;
+            }
         }
-
-        if (first && !second) {
-            return n != m;
+        else if((arr1[0].equals("a")||arr1[0].equals("c")||arr1[0].equals("e")||arr1[0].equals("g"))&&(arr2[0].equals("b")||arr2[0].equals("d")||arr2[0].equals("f")||arr2[0].equals("h"))){
+            if(n!=m){
+                return true;
+            }
         }
-
-        return n != m;
+        else if((arr1[0].equals("b")||arr1[0].equals("d")||arr1[0].equals("f")||arr1[0].equals("h"))&&(arr2[0].equals("a")||arr2[0].equals("c")||arr2[0].equals("e")||arr2[0].equals("g"))){
+            if(n!=m){
+                return true;
+            }
+        }
+        
+      return false;
     }
 }
