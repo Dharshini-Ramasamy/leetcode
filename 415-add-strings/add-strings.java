51cb924061563161b5b7ddc6d2e1da5a85ad4da6
Dharshini-Ramasamy <dharshiniramasamyy@gmail.com>
2026-09-17 21:32:43 +0530

    Time: 1 ms (100.00%) | Memory: 43.8 MB (59.45%) - LeetSync

--- 415-add-strings/add-strings.java
+++ 415-add-strings/add-strings.java
@@ -1,24 +1,37 @@
 class Solution {
     public String addStrings(String num1, String num2) {
-        int i=num1.length()-1;
-        int j=num2.length()-1;
-        int carry=0;
-        String m="";
-        while(i>=0||j>=0||carry!=0){
-            int dig1=0;
-            int dig2=0;
-            if(i>=0){
-             dig1=num1.charAt(i)-'0';
+
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
+
+        char[] result = new char[Math.max(num1.length(), num2.length()) + 1];
+        int k = result.length - 1;
+
+        while (i >= 0 || j >= 0 || carry != 0) {
+
+            int dig1 = 0;
+            int dig2 = 0;
+
+            if (i >= 0) {
+                dig1 = num1.charAt(i) - '0';
             }
-            if(j>=0){
-             dig2=num2.charAt(j)-'0';
+
+            if (j >= 0) {
+                dig2 = num2.charAt(j) - '0';
             }
-            int sum=dig1+dig2+carry;
-            carry=sum/10;
-            m=(sum%10)+m;
+
+            int sum = dig1 + dig2 + carry;
+
+            result[k] = (char)('0' + (sum % 10));
+
+            carry = sum / 10;
+
             i--;
-            j--; 
+            j--;
+            k--;
         }
-        return m;
+
+        return new String(result, k + 1, result.length - k - 1);
     }
 }
